63f15feaf358937aad3cd809c91539353bd958b0
samenc99 <mateus.enc@gmail.com>
2021-05-03 20:23:06 -0300

    mudando a extensão da página para jsx

--- src/Pages/Feed/FeedPage.jsx
+++ src/Pages/Feed/FeedPage.jsx
@@ -4,6 +4,6 @@ import useRequestData from "../../Hooks/useRequestData";
 export default function FeedPage() {
   const [restaurants] = useRequestData('/restaurants', [], 'restaurants')
 
-  console.log('restaurants', restaurants)
+
   return <h1>Feed</h1>;
 }
