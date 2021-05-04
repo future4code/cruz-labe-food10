71811e7ed20e7ded24c68ef47c5834f901dc981f
samenc99 <mateus.enc@gmail.com>
2021-05-04 18:00:25 -0300

    Criei o card restaurante e testei no CartPage

--- src/Pages/CartPage/CartPage.js
+++ src/Pages/CartPage/CartPage.js
@@ -1,14 +1,31 @@
 import React, { useContext } from "react";
 import  GlobalStateContext  from "../../Global/GlobalStateContext";
+import CardRestaurant from "../../Components/CardRestaurant/CardRestaurant";
 
 export default function CartPage() {
   const {restaurantDetailsData} = useContext(GlobalStateContext);
 
+  //enquanto não tem cart com produtos no global state
+  const cart=[
+    {
+      id:1, //id do restaurante
+      products:[
+        {
+          id:'bEj2JorVLWo86iJf7OF9',
+          quantity: 2
+        },
+        {
+          id:'fMMfstMTxeos8NWTS4j1',
+          quantity: 3
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
       <h1>Carrinho</h1>
-
+      <CardRestaurant id={cart[0].id} page={'Cart'} />
     </div>
   );
 }
