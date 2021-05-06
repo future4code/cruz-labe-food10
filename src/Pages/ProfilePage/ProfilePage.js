47b9791f46d32bc03a22f74ce99099b5bf45f7f6
Giselle Rosa <gisellenrosa@gmail.com>
2021-05-06 12:47:02 -0300

    testing remover button and add feed footer

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Footer from '../../Components/Footer/Footer'
+
 
 export default function ProfilePage() {
-  return <h1>Perfil</h1>;
+  return (
+    <div>
+      <h1>Perfil</h1>
+      <Footer/>
+    </div>
+  );
 }
